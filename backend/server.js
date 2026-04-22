b938c772f43996ab39cdbd440b35c6e694146d19
NikunjMakwana01 <nikunjmakwana1018@gmail.com>
2026-04-22 16:00:27 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,8 @@ app.use("/api", (req, res, next) => {
 // CORS
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "*",
+    // process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
